d83561ab79923777930a05f5a62127113ce273df
Jacob Jennings <jacob_a_jennings@icloud.com>
2024-03-05 17:06:42 +0000

    Final game

--- Flag Guessing Game/ContentView.swift
+++ Flag Guessing Game/ContentView.swift
@@ -8,52 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "spain", "uk", "ukraine", "us"].shuffled()
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...3)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var Score = 0
     
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                    
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                    
-                }
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        scoreTitle = "Correct"
-                    } label: {
-                        Image(countries[number])
+                Spacer()
+                
+                Text("Guess the flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.heavy))
+                        
+                    }
+                    
+                    
+                    ForEach(0..<4) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                Text("Score: \(Score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+                
+            }
+            .padding()
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text(scoreMessage)
             }
         }
     }
-    
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-        } else {
-            scoreTitle = "Wrong"
+        func flagTapped(_ number: Int) {
+            if number == correctAnswer {
+                scoreTitle = "Correct"
+                Score=Score+1
+                scoreMessage="Your score is \(Score)"
+                
+            } else {
+                scoreTitle = "Wrong"
+                scoreMessage="That's the flag of \(countries[number])"
+                
+            }
+            
+            showingScore = true
         }
         
-        showingScore = true
-    }
-}
+        func askQuestion() {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...3)
+        }
+    
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
+}
+
+
 
 
