365e429fa49263f8f19ac00d32a84b73f9886b12
Jacob Jennings <jacob_a_jennings@icloud.com>
2024-03-05 10:35:56 +0000

    Initial Commit

---
+++ Flag Guessing Game/Flag_Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_Guessing_GameApp.swift
+//  Flag Guessing Game
+//
+//  Created by Tesco Labs on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flag_Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
