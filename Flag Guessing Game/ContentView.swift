8230aa531c676286da45e790242b1ca7baa067bc
Jacob Jennings <jacob_a_jennings@icloud.com>
2024-03-06 10:03:18 +0000

    Changed from 4 to 3 flags, added spacers to fit other phone models better, changed colours to work well in light and dark mode

--- Flag Guessing Game/ContentView.swift
+++ Flag Guessing Game/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...3)
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
@@ -19,8 +19,8 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.70), location: 0.3),
+                .init(color: Color(red: 0.90, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
             
@@ -31,10 +31,10 @@ struct ContentView: View {
                 Text("Guess the flag")
                     .font(.largeTitle.weight(.bold))
                     .foregroundStyle(.white)
+                Spacer()
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
-                            .foregroundStyle(.black)
                             .font(.subheadline.weight(.heavy))
                         
                         Text(countries[correctAnswer])
@@ -43,7 +43,7 @@ struct ContentView: View {
                     }
                     
                     
-                    ForEach(0..<4) { number in
+                    ForEach(0..<3) { number in
                         Button {
                             flagTapped(number)
                         } label: {
@@ -55,7 +55,7 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
-                .background(.regularMaterial)
+                .background(.ultraThickMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 Spacer()
@@ -91,7 +91,7 @@ struct ContentView: View {
         
         func askQuestion() {
             countries.shuffle()
-            correctAnswer = Int.random(in: 0...3)
+            correctAnswer = Int.random(in: 0...2)
         }
     
     
